2f4af37d746210baca8a5ceb4b2add34984fba1f
Chowdhury-Anik <bloggeranik1986@gmail.com>
2021-04-02 22:36:45 +0600

    done

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-const uri = `mongodb+srv://BongoVandar:test1234@cluster0.kntbw.mongodb.net/bdbazar?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pbmsk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-// console.log("password testing", process.env.DB_USER, process.env.DB_PASS, process.env.DB_NAME);
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
